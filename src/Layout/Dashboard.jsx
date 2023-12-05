75b04b100608252ad106b32e6d5cf50b8df415cf
ashraf56 <ashrafulfahim07@gmail.com>
2023-12-05 10:25:47 +0600

    random update

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -4,9 +4,10 @@ import img from '../assets/icons8-question-mark-64.png'
 import { GoStack } from "react-icons/go";
 import { IoLogOutOutline } from "react-icons/io5";
 import { IoStatsChartOutline } from "react-icons/io5";
-import { RiMoneyDollarCircleFill } from "react-icons/ri";
 import { GiUbisoftSun } from "react-icons/gi";
-
+import { FaChevronDown } from 'react-icons/fa';
+import { MdAttachMoney } from "react-icons/md";
+import { AiOutlineHome } from "react-icons/ai";
 const Dashboard = () => {
 
     return (
@@ -26,24 +27,32 @@ const Dashboard = () => {
    <h1 className=' text-[#1570EF] font-mono font-bold text-center pt-5 flex items-center justify-center '><GiUbisoftSun />  <span className='px-2 text-xl '>Tajdid Store </span></h1>
 </div>
 
-    <div className='grid grid-cols-1   gap-0 pt-5 text-sm font-semibold  '>
-    <span  className='  hover:bg-[#EFF8FF] border-s-4 border-[#EFF8FF] hover:border-s-4  hover:border-[#1570EF] p-3 '>
-    <NavLink to={'/'}  className='flex items-center '> <GoStack /> <span className='px-2 ac'>Products</span></NavLink>
-    </span>
-    <span className='  hover:bg-[#EFF8FF] border-s-4 hover:border-s-4 border-[#EFF8FF] hover:border-[#0059CF] p-3 '>
-    <NavLink className='flex items-center'> <IoStatsChartOutline /> <span className='px-2'>Analytics</span></NavLink>
-    </span>
-    <span className='hover:bg-[#EFF8FF] border-s-4 hover:border-s-4 border-[#EFF8FF] hover:border-[#1570EF] p-3 '>
-    <NavLink  className='flex items-center'> <GoStack /> <span className='px-2'>Billing</span></NavLink>
-    </span>
-    <span className=' hover:bg-[#EFF8FF] border-s-4 hover:border-s-4 border-[#EFF8FF] hover:border-[#1570EF] p-3  '>
-    <NavLink className='flex items-center'> <RiMoneyDollarCircleFill /> <span className='px-2'>Products</span></NavLink>
-    </span>
-    <span className=' hover:bg-[#EFF8FF] border-s-4 hover:border-s-4 border-[#EFF8FF] hover:border-[#1570EF] p-3 '>
+    <ul className='grid grid-cols-1  gap-0 pt-5 text-sm font-semibold  '>
+    <li  className='  bg-[#EFF8FF] border-s-4 hover:border-s-4  border-[#1570EF] p-3 '>
+    <NavLink to={'/'}   className='flex items-center '> <GoStack /> <span className='px-2 ac'>Products</span></NavLink>
+    </li>
+    <li className='  hover:bg-[#EFF8FF] border-s-4 hover:border-s-4 border-[#EFF8FF] hover:border-[#0059CF] p-3 '>
+    <NavLink className='flex items-center'> <IoStatsChartOutline /> <span className='px-2'>Analytics</span>
+    <FaChevronDown />
+    </NavLink>
+  
+
+    
+    </li>
+    <li className='hover:bg-[#EFF8FF] border-s-4 hover:border-s-4 border-[#EFF8FF] hover:border-[#1570EF] p-3 '>
+    <NavLink  className='flex items-center'> <MdAttachMoney /> <span className='px-2'>Billing</span>
+    
+    <FaChevronDown />
+    </NavLink>
+    </li>
+    <li className=' hover:bg-[#EFF8FF] border-s-4 hover:border-s-4 border-[#EFF8FF] hover:border-[#1570EF] p-3  '>
+    <NavLink className='flex items-center'> <AiOutlineHome /> <span className='px-2'>Company</span></NavLink>
+    </li>
+    <li className=' hover:bg-[#EFF8FF] border-s-4 hover:border-s-4 border-[#EFF8FF] hover:border-[#1570EF] p-3 '>
     <NavLink className='flex items-center' > <IoLogOutOutline /><span className='px-2'>Log out</span></NavLink>
-    </span>
+    </li>
    
-    </div>
+    </ul>
 
 <div className=' absolute  bottom-1  px-2'>
 <div className="card max-w-full  bg-[#E0FFFF]">
